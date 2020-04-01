d732d6925d667b0e592bb55f105d804694fa93e4
Sahaj Khandelwal <sahaj@appointy.com>
2020-04-01 19:52:52 +0530

    Replaced eventemitter with subject

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
 import { Ingredient } from "../shared/ingredient.model";
-import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient("Apple", 10),
     new Ingredient("Banana", 5)
@@ -12,10 +12,10 @@ export class ShoppingListService {
   }
   addIngredient(ingr: Ingredient) {
     this.ingredients.push(ingr);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
   addIngredients(ingrs: Ingredient[]) {
     this.ingredients.push(...ingrs);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
